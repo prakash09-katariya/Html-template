cdaa0258015725fae3d7db6bc34918d2f6ca4e70
unknown <prakashkatariya288@gmail.com>
2025-04-08 18:21:08 +0530

    df

--- gruham-template/js/script.js
+++ gruham-template/js/script.js
@@ -6,6 +6,8 @@ function check(){
     document.getElementsByTagName("a")[2].style.color="#ffffff"
     document.getElementsByTagName("a")[3].style.color="#ffffff"
     document.getElementsByTagName("a")[4].style.color="#ffffff"
+    document.getElementsByTagName("a")[5].style.color="#ffffff"
+    document.getElementsByTagName("a")[6].style.color="#ffffff"
     document.getElementsByTagName('img')[1].src = 'https://www.evolutionmoney.co.uk/wp-content/uploads/2020/03/83437634_m-1.jpg';
     document.getElementsByTagName('img')[2].src = 'https://img.freepik.com/free-photo/high-angle-man-working-as-real-estate-agent_23-2151065010.jpg?t=st=1744092753~exp=1744096353~hmac=5c0db708e26cf57d9b417d9799ee90528081e5d145c4d6a7fdde3a6eb9c5958f&w=826';
     document.getElementsByTagName('img')[3].src = 'https://img.freepik.com/free-photo/high-rise-office-buildings_1359-640.jpg?t=st=1744092894~exp=1744096494~hmac=abcbcb9091e5fcc60a96542b26ded09f0a773124c84efb8afed33d6dfbc7b2a3&w=740';
@@ -20,6 +22,8 @@ function prive(){
     document.getElementsByTagName("a")[2].style.color="#ffffff"
     document.getElementsByTagName("a")[3].style.color="#ffffff"
     document.getElementsByTagName("a")[4].style.color="#ffffff"
+    document.getElementsByTagName("a")[5].style.color="#ffffff"
+    document.getElementsByTagName("a")[6].style.color="#ffffff"
     document.getElementsByTagName('img')[1].src = 'https://metropolitan.realestate/wp-content/uploads/2021/12/dubai-property-market-trends-forecast-for-2022-1.jpg';
 
 }
