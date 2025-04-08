14033f82abb98827c8147361b2e241e4168213a8
unknown <prakashkatariya288@gmail.com>
2025-04-08 12:00:29 +0530

    newtask

---
+++ gruham-template/js/script.js
@@ -0,0 +1,19 @@
+function check(){
+    document.body.style.backgroundColor = "#000000";
+    document.getElementsByTagName('body').innerTex="hello-google";
+    document.getElementsByTagName("a")[0].style.color="#ffffff"
+    document.getElementsByTagName("a")[1].style.color="#ffffff"
+    document.getElementsByTagName("a")[2].style.color="#ffffff"
+    document.getElementsByTagName("a")[3].style.color="#ffffff"
+    document.getElementsByTagName("a")[4].style.color="#ffffff"
+    document.getElementsByTagName('img')[1].src = 'https://www.evolutionmoney.co.uk/wp-content/uploads/2020/03/83437634_m-1.jpg';
+    document.getElementsByTagName('img')[2].src = 'https://img.freepik.com/free-photo/high-angle-man-working-as-real-estate-agent_23-2151065010.jpg?t=st=1744092753~exp=1744096353~hmac=5c0db708e26cf57d9b417d9799ee90528081e5d145c4d6a7fdde3a6eb9c5958f&w=826';
+    document.getElementsByTagName('img')[3].src = 'https://img.freepik.com/free-photo/high-rise-office-buildings_1359-640.jpg?t=st=1744092894~exp=1744096494~hmac=abcbcb9091e5fcc60a96542b26ded09f0a773124c84efb8afed33d6dfbc7b2a3&w=740';
+    document.getElementsByTagName('img')[9].src = 'https://dfy-properties.com/wp-content/uploads/2023/05/handshake_with_homes.jpg';
+    document.getElementsByTagName('img')[10].src = 'https://avatars.mds.yandex.net/i?id=f6efbece180282da39dad7c757b28d4f9a4e7a5b-3322605-images-thumbs&n=13';
+}
+
+function showAlert() {
+    alert("thank you click press on ok");
+    prompt("plese enter you idea")
+  } 
